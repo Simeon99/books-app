2431f980bd865a4ba590d11467d502637aeea544
Simeon99 <simeonilic@yahoo.com>
2022-01-17 20:19:21 +0100

    adding collcetion preview component and colection item component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import './App.css';
 import {Route, Link } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-
-const HatsPage = props =>{
-  console.log(props);
-  return(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  )
-};
 
 
 function App() {
@@ -19,7 +11,7 @@ function App() {
     <div>
           
           <Route exact path='/' component={HomePage}/>
-          <Route  path='/shop/hats' component={HatsPage}/>
+          <Route  path='/shop' component={ShopPage}/>
         
     </div>
   );
